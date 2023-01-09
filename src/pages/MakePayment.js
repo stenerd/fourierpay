fcf5415fe6c0238ef289475c2358524a4b13df39
Chinedu Ifediorah <115559970+justmeeky@users.noreply.github.com>
2023-01-09 09:08:55 +0100

    paystack payment

--- src/pages/MakePayment.js
+++ src/pages/MakePayment.js
@@ -12,7 +12,7 @@ import dayjs from 'dayjs';
 import { FormControl, Grid, InputLabel, MenuItem, Select, TextField, Divider } from '@mui/material';
 import DashboardLayout from '../components/DashboardLayout';
 import Titlebar from '../components/TitleBar';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Fields from '../components/Fields';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
@@ -20,198 +20,251 @@ import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios'
 import SendIcon from '@mui/icons-material/Send';
 import Protected from '../utils/axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { PaystackButton, PaystackConsumer } from 'react-paystack'
 
 
 
 
 
 const MakePayment = () => {
-    const navigate = useNavigate()
+    let { code } = useParams();
+    const dispatch = useDispatch()
     const [loading, setLoading] = React.useState(false);
-    const [state, setState] = React.useState({
-        name: '',
-        description: '',
-        amount: '',
-        expected_number_of_payment: '',
-        expires_at: ''
-    })
-    const [stateError, setStateError] = React.useState('')
-    const [selectedFields, setSelectedFields] = React.useState([
-        {
-            field_name: '',
-            field_type: '',
-            required: 'false',
-            options: []
-        }
-    ])
-    const [selectedFieldsError, setSelectedFieldsError] = React.useState([''])
+    const [value, setValue] = React.useState(0);
+    const [paymentData, setPaymentData] = React.useState({});
+    const [paymentLink, setPaymentLink] = React.useState({});
+    const paystackButtonRef = React.useRef(null);
 
-  
-    const handleChanges = (e) => {
-        setState((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    const FetchPaymentLink = async () => {
+        try {
+            const response = await axios.get(`http://localhost:4000/api/payment-link/${code}`)
+            console.log('ppp >> ', response.data.data)
+            setPaymentLink(response.data.data)
+           
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
+    React.useEffect(()=>{
+        FetchPaymentLink()
+    },[])
 
     const handleFieldChanges = (e, index) => {
-        setSelectedFields((prev) => {
-            let name = ''
-            if (index >= 1) {
-                name = e.target.name.replace("" + index, "");
-            } else {
-                name = e.target.name
-            }
-            let new_data = [...prev]
+        e.preventDefault();
+        e.stopPropagation();
+        setPaymentLink((prev) => {
+            let new_data = [...prev.form]
             new_data[index] = {
                 ...new_data[index],
-                [name]: e.target.value
+                'answer': e.target.value
             }
-            return new_data;
+
+            if (e.target.value) {
+                delete new_data[index].error
+            }
+            return {
+                ...prev,
+                form: new_data
+            };
         })
     }
 
     const validateInput = () => {
-        setStateError('')
-        let empty_data = []
-        for (let k = 0; k < selectedFields.length; k++) {
-            empty_data.push('')
-        }
-        setSelectedFieldsError(empty_data)
-        if (!state.name || !state.amount || !state.description ) {
-            setStateError('* name, amount and description are required fields.')
-            return false
-        }
+
         let check = true
 
-        for (let i = 0; i < selectedFields.length; i++) {
-            const each = selectedFields[i];
+        const validatedForm = []
 
-            if (!each.field_name || !each.field_type) {
-                check = false;
-                setSelectedFieldsError((prev) => {
-                    let data = [...prev]
-                    data[i] = '* field name, required and field type are required field'
-                    return data
-                })
+        for (let i = 0; i < paymentLink.form.length; i++) {
+            const each = {...paymentLink.form[i]};
+
+            delete each.error;
+
+            if (each.required && !each.answer) {
+                each.error = `${each.field_name} is required`
+                check = false
             }
 
-            if (each.field_type === 'select' && !each.options.length) {
-                check = false;
-                setSelectedFieldsError((prev) => {
-                    let data = [...prev]
-                    data[i] = '* select field is choosen for field type but no option was selected.'
-                    return data
-                })
-            }
-            
+            validatedForm.push(each)
+
         }
+
+        setPaymentLink(prev => ({
+            ...prev,
+            form: validatedForm
+        }))
+        
         return check
     }
 
-    const handleRemoveField = (e, index) => {
-        const data = [...selectedFields]
-        data.splice(index, 1)
-        setSelectedFields(data)
+    const config = {
+        ...paymentData
     };
 
-    const handleRemoveFieldOption = (e, index1, index2) => {
-        const data = [...selectedFields]
-        data[index1].options.splice(index2, 1)
-
-        setSelectedFields(data)
-    };
-
-    const onKeyDownHandler = (id, index) => {
-        const element = document.getElementById(id)
-        if (element.value === '') {
-            return
-        }
-        const data = [...selectedFields]
-        data[index].options.push(element.value)
-        document.getElementById(id).value = ''
-        setSelectedFields(data)
-    };
-
-    const createLink = async (e) => {
-        if (e.keyCode === 13) {
-            console.log('heddd')
-            return
-        }
-        e.preventDefault()
-        console.log('processing....')
-        setLoading(true)
-        const check = validateInput()
-        if (!check) {
-            setLoading(false)
-            return;
-        }
-        const payload = {
-            ...state,
-            amount: Number(state.amount),
-            expires_at: new Date(state.expires_at),
-            expected_number_of_payments: Number(state.expected_number_of_payment),
-            form: selectedFields.map(e => ({
-                ...e,
-                required: e.required === 'true' ? true : false,
-            }))
-        }
+    // you can call this function anything
+    const handleSuccess = (reference) => {
+        // Implementation for whatever you want to do with reference and after success call.
+        console.log(reference);
         try {
-            await Protected.post(`http://localhost:4000/api/payment-link/create`, payload)
-
-            console.log('done successfully')
-            setLoading(false)
-            toast.success('Link Created!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-            navigate('/dashboard/paymentlinks')
+            axios.post(`http://localhost:4000/api/payment/verify`, {
+                reference: paymentData.reference
+            })
         } catch (error) {
             console.log(error.response.data.message)
             toast.error(error.response.data.message)
             console.log('An error occurred')
             setLoading(false)
         }
-        console.log(state)
+    };
+
+    // you can call this function anything
+    const handleClose = (reference) => {
+        // implementation for  whatever you want to do when the Paystack dialog closed.
+        console.log('closed >> ', paymentData, reference)
+
+        try {
+            axios.put(`http://localhost:4000/api/payment/abandon`, {
+                reference: paymentData.reference
+            })
+        } catch (error) {
+            console.log(error.response.data.message)
+            toast.error(error.response.data.message)
+            console.log('An error occurred')
+            setLoading(false)
+        }
+    }
+
+    const componentProps = {
+        ...config,
+        text: 'Paystack Button Implementation',
+        onSuccess: (reference) => handleSuccess(reference),
+        onClose:(reference) => handleClose(reference)
+    };
+
+
+    const openPaystack = async (e, initializePayment) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        console.log("paymentData >> ", paymentData)
+
+        initializePayment(handleSuccess, handleClose)
+
+    }
+    
+    const makePaymentHandler = async (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        const check = validateInput()
+        if (!check) {
+            setLoading(false)
+            return;
+        }
+        try {
+            const initiateTrnx = await axios.post(`http://localhost:4000/api/payment/initialize`, {
+                amount: paymentLink.amount,
+                payment_link_id: paymentLink._id,
+                form: paymentLink.form
+            })
+
+            console.log('initiateTrnx >> ', initiateTrnx.data.data)
+            
+            setPaymentData((prev) => ({...initiateTrnx.data.data}))
+            setValue((prev => prev ? 0 : 1))
+
+            setTimeout(() => {
+                paystackButtonRef.current.click()
+            }, 2000);
+
+            
+
+            
+
+            setLoading(false)
+
+            // console.log('done successfully')
+            // ref.current.open();
+            // setLoading(false)
+            // toast.success('Link Created!', {
+            //     position: "top-right",
+            //     autoClose: 5000,
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     pauseOnHover: true,
+            //     draggable: true,
+            //     progress: undefined,
+            //     theme: "light",
+            // });
+        } catch (error) {
+            console.log(error.response.data.message)
+            toast.error(error.response.data.message)
+            console.log('An error occurred')
+            setLoading(false)
+        }
 
     }
 
     return (
         <>
              <div className='min-h-screen'>
-                <div className='px-16 py-16 mx-auto'>
+                <div className='px-4 lg:px-16 py-8 lg:py-16 mx-auto'>
                     <div className='flex mx-auto min-h-[85vh]'>
 
                     
-                        <div className='w-[55%] mx-auto c-make-payment'>
+                        <div className='w-[90%] lg:w-[55%] mx-auto c-make-payment p-[1.5rem] lg:p-[4rem]'>
                             <div className='flex flex-col justify-center items-center'>
                                 <div className='w-full'>
                                     <form className='w-full'>
                                         <div className='flex justify-between'>
                                             <h3 className='text-xl mb-16 font-bold home c-auth-title'>Pay</h3>
                                             <div>
-                                                <small className='text-sm text-[#00bf00] status-pill c-status-border-pill'>Active - 24th March 2023</small>
+                                                <small className='text-sm text-[#00bf00] status-pill c-status-border-pill'>{paymentLink.status} {paymentLink.expires_at && ' - ' + paymentLink.expires_at}</small>
                                             </div>
                                         </div>
                                         
-                                        <p className='font-bold text-gray-700 text-lg'>Departmental Dues</p>
-                                        <span className='font-bold text-gray-500 inline-block w-[70%]'>Dues for all members of the department</span>
-                                        <p className='font-bold text-gray-700 text-lg mt-4'>$5000</p>
-                                        <div className='mt-8 mb-8'>
-                                            {stateError && <small className='text-red-600'> {stateError}</small>}
-                                            <Grid container spacing={2}>
-                                                <Grid item xs={12} md={6}>
-                                                    <div className='flex flex-col space-y-3 mb-8'>
-                                                        <label className='text-sm font-bold block mt-0 mb-0 text-gray-700'>Name</label>
-                                                        <input placeholder='Name' name='name' onChange={handleChanges} className="py-2 px-4 w-full outline-none c-text-input" />
-                                                    </div>
-                                                </Grid>
-                                            </Grid>
-                                        </div>
+                                        <p className='font-bold text-gray-700 text-lg'>{paymentLink.name}</p>
+                                        <span className='font-bold text-gray-500 inline-block w-[70%]'>{paymentLink.description}</span>
+                                        <p className='font-bold text-gray-700 text-lg mt-4'>${paymentLink.amount}</p>
+                                        {
+                                            paymentLink.form && paymentLink.form.length ? (
+                                                <div className='mt-8 mb-8'>
+                                                    <Grid container spacing={2}>
+                                                        {
+                                                            paymentLink.form.map((link, index) => (
+                                                                <Grid item xs={12} md={6} key={index}>
+                                                                    <div className='flex flex-col space-y-3 mb-8'>
+                                                                        <label className='text-sm font-bold block mt-0 mb-0 text-gray-700'>{link.field_name}</label>
+                                                                        {
+                                                                            link.field_type == 'text' ? (
+                                                                                <input required placeholder={link.field_name} name={link.field_name + index} onChange={(e) => handleFieldChanges(e, index)} className="py-2 px-4 w-full outline-none c-text-input" />
+                                                                            ) : (
+                                                                                <select placeholder={link.field_name} name={link.field_name + index} onChange={(e) => handleFieldChanges(e, index)} className="py-2 px-4 w-full outline-none c-text-input">
+                                                                                    <option value={''}>Select {link.field_name} </option>
+                                                                                    {
+                                                                                        link.options.map((option, i) => (
+                                                                                            <option key={option + i} value={option}>{option} </option>
+                                                                                        ))
+                                                                                    }
+                                                                                </select>
+                                                                            )
+                                                                        }
+                                                                        {
+                                                                            link.error ? (
+                                                                                <small className='c-pay-error'>{link.error}</small>
+                                                                            ) : ''
+                                                                        }
+                                                                    </div>
+                                                                </Grid>
+                                                            ))
+                                                        }
+                                                    </Grid>
+                                                </div>
+                                            ) : ''
+                                        }
+                                        
                                         
 
                                         
@@ -223,9 +276,22 @@ const MakePayment = () => {
                                             <span className='bg-[#0d1510] cursor-pointer py-3 px-4 w-2/5  rounded-md text-white' onClick={generateField}>Generate Fields</span>
                                         </div> */} 
                                         <div className='py-4'>
-                                            <button className='c-primary-button' onClick={createLink}>
+                                            <button className='c-primary-button'  onClick={(e) => makePaymentHandler(e)}>
                                                 {loading ? 'Paying...' : 'Make Payment'}
                                             </button>
+
+                                            <PaystackConsumer 
+                                                className='hidden'
+                                                onSuccess = {(reference) => handleSuccess(reference)}
+                                                onClose = {(reference) => handleClose(reference)}
+                                                {...paymentData}
+                                            >
+                                                {({initializePayment}) => <button className='hidden' ref={paystackButtonRef} onClick={(e) => openPaystack(e, initializePayment)}>
+                                                    {loading ? 'Paying...' : 'Make Payment'}
+                                                </button>}
+                                            </PaystackConsumer>
+
+
                                         </div>
                                     </form>
                                 </div>
