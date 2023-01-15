05471cc010956f6bc0042e84d270760e2e06b38b
Chinedu Ifediorah <115559970+justmeeky@users.noreply.github.com>
2023-01-15 11:37:17 +0100

    save changes

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,7 +6,7 @@ const Section = () => {
     return (
         <>
             <div className='bg-gray-100'>
-                <div className='pt-16 pb-8'>
+                <div className='pt-16 pb-16'>
                     <div className="w-4/5 mx-auto flex justify-between items-center">
                         <div>
                             <h1 className='text-2xl text-center' style={{fontWeight: '900'}}>250K</h1>
@@ -27,7 +27,7 @@ const Section = () => {
                 <div className="py-8 divide-y-2 divide-gray-300">
                   <Divider/>
                 </div>
-                <div className='w-4/5 py-4 mx-auto flex justify-between items-center gap-3'>
+                <div className='w-4/5 pt-4 pb-16 mx-auto flex justify-between items-center gap-3'>
                       <div className='flex-1'>
                            <h1 className='text-[46px] font-bold'>Financial experience built for tomorrow</h1>
                       </div>
@@ -36,6 +36,84 @@ const Section = () => {
                            <h2 className='text-xl'>Fourier Pay was built from scratch to inspire embedded Financial experience. We Provide you products and tools you need to grow your revenue and collect payment instantly</h2>
                            <button className='bg-[#1f332b] text-white py-4 px-6 rounded-md'>Learn More</button>
                       </div>
+                </div>
+            </div>
+
+            <div className='pt-16 pb-16 bg-[#ebefe6] mx-auto'>
+                <div className='bg-[#ebefe6] w-[65%] mx-auto'>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <div className='pt-16 text-center font-bold'>How It Works</div>
+                        </Grid>
+                        <Grid  item xs={6}>
+                            <div className='px-10 py-12 bg-white c-how-it-works-icon'>
+                                <div className='relative mb-12'>
+                                    <span className='left'></span>
+                                    <span className='right'></span>
+                                </div>
+                                <div>
+                                    We don't just help you pay for anything—we help you get paid! And we make it easy for anyone who wants to pay to do so by creating links that they can access directly through their email inboxes.
+                                </div>
+                                
+                            </div>
+                        </Grid>
+                        <Grid  item xs={6}>
+                            <div className='px-10 py-12 bg-white c-how-it-works-icon'>
+                                <div className='relative mb-12'>
+                                    <span className='left'></span>
+                                    <span className='right'></span>
+                                </div>
+                                <div>
+                                    We don't just help you pay for anything—we help you get paid! And we make it easy for anyone who wants to pay to do so by creating links that they can access directly through their email inboxes.
+                                </div>
+                                
+                            </div>
+                        </Grid>
+                        <Grid  item xs={6}>
+                            <div className='px-10 py-12 bg-white c-how-it-works-icon'>
+                                <div className='relative mb-12'>
+                                    <span className='left'></span>
+                                    <span className='right'></span>
+                                </div>
+                                <div>
+                                    We don't just help you pay for anything—we help you get paid! And we make it easy for anyone who wants to pay to do so by creating links that they can access directly through their email inboxes.
+                                </div>
+                                
+                            </div>
+                        </Grid>
+                        <Grid  item xs={6}>
+                            <div className='px-10 py-12 bg-white c-how-it-works-icon'>
+                                <div className='relative mb-12'>
+                                    <span className='left'></span>
+                                    <span className='right'></span>
+                                </div>
+                                <div>
+                                    We don't just help you pay for anything—we help you get paid! And we make it easy for anyone who wants to pay to do so by creating links that they can access directly through their email inboxes.
+                                </div>
+                                
+                            </div>
+                        </Grid>
+                    </Grid>
+                </div>
+                <div className='bg-[#ebefe6] w-4/5 mx-auto mt-24'>
+                    
+                    <p className='text-6xl font-medium leading-normal mb-8'>
+                        Accept, manage and make payments with fourierpay fast and easy!
+                    </p>
+                    <p className='text-xl leading-9 font-medium text-gray-700 mb-4'>
+                    Fourierpay is a payment platform that gives you the ability to pay for products through a link.it also helps you create payment links that can be accessed by whoever you want payment from thereby enabling mass payments.
+                    </p>
+                    <p className='text-xl leading-9 font-medium text-gray-700 mb-4'>
+                        We have done the core payment integrations and abstractions, so your team can easily access and mange payments with our APIs and access multiple payment functionalities.
+                    </p>
+                    <p className='text-base leading-9 font-medium text-gray-700 mb-4'>
+                        <ul className='list-inside' style={{listStyle: 'inside'}}>
+                            <li>Quick transfers</li>
+                            <li>Initiate one-time and recurring payments</li>
+                            <li>Secure payment verification</li>
+                            <li>Customer verification</li>
+                        </ul>
+                    </p>
                 </div>
             </div>
 
