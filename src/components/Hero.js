c87183dba8be80a04b86aac44108c935929948a7
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-02-14 16:00:41 +0100

    gsap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,91 @@
 import { Avatar, Grid } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Hero.css'
+import { gsap, Power3, Ease, SteppedEase } from 'gsap'
+import { duration } from 'moment'
+import { TextPlugin } from 'gsap/all'
+
+gsap.registerEffect(TextPlugin);
 const Hero = () => {
+    
+    let firstRef = useRef(null)
+    let secondRef = useRef(null)
+    let thirdRef = useRef(null)
+    const buttonRef = useRef(null)
+    const t1 = gsap.timeline()
+    const typewriter = useRef(null)
+
+    useEffect(() => {
+
+        // var t2 = gsap.timeline({
+        //     paused:true
+        //   });
+        //   // letter animation
+        //   tl.fromTo(".anim-typewriter", 8, {
+        //     width: "0",
+        //   }, {
+        //     width: "20.18em", /* same as CSS .line-1 width */
+        //     ease:  SteppedEase.config(37)
+        //   }, 0);
+          // text cursor animation
+        //   tl.fromTo(".anim-typewriter", 0.5, {
+        //     "border-right-color": "rgba(255,255,255,0.75)"
+        //   }, {
+        //     "border-right-color": "rgba(255,255,255,0)",
+        //     repeat: -1,
+        //     ease:  SteppedEase.config(37)
+        //   }, 0);
+          
+        //   tl.play();
+
+        var t2 = gsap.timeline({
+            paused:true
+          });
+          // letter animation
+          t2.from(".anim-typewriter", 8, {
+            width: "0",
+          }, {
+            width: "20.18em", /* same as CSS .line-1 width */
+            ease:  SteppedEase.config(37)
+          }, 0);
+          // text cursor animation
+          t2.from(".anim-typewriter", 0.5, {
+            "border-right-color": "rgba(255,255,255,0.75)"
+          }, {
+            "border-right-color": "rgba(255,255,255,0)",
+            repeat: -1,
+            ease:  SteppedEase.config(37)
+          }, 0);
+          
+          t2.play();
+
+          gsap.from(typewriter.current, {text:"All in one platform for accepting payment and embedded fintech experience to fast track your payment", ease:"power1.in", duration:2, repeat:10, yoyo:true, repeatDelay:0.4})
+
+        t1.from(
+            [firstRef.current, secondRef.current, thirdRef.current],
+            {
+                stagger: {
+                    amount: .6
+                },
+                opacity: 0,
+                x: 20,
+                duration:1.2
+            }).from(buttonRef.current,{
+                y:30,
+                opacity:0,
+                duration:.6
+            })
+              // text cursor animation
+            //   tl.fromTo(".anim-typewriter", 0.5, {
+            //     "border-right-color": "rgba(255,255,255,0.75)"
+            //   }, {
+            //     "border-right-color": "rgba(255,255,255,0)",
+            //     repeat: -1,
+            //     ease:  SteppedEase.config(37)
+            //   }, 0);
+            t2.play()
+    }, [])
     return (
         <>
             <div className="bg-[#1f332b] min-h-screen md:min-h-[90vh] max-w-[100vw]">
@@ -25,17 +108,18 @@ const Hero = () => {
                     </div>
                     <div className='min-h-[80vh] md:min-h-[75vh] flex justify-center items-center mx-auto'>
                         <div className='flex justify-center items-center'>
-                            <Grid container justifyContent={{xs:'center'}} alignItems='center' spacing={4} className='w-full' style={{marginLeft: 0}}>
+                            <Grid container justifyContent={{ xs: 'center' }} alignItems='center' spacing={4} className='w-full' style={{ marginLeft: 0 }}>
                                 <Grid item xs={12} md={6} style={{ paddingLeft: '0' }}>
                                     <div className='space-y-8 w-full'>
                                         <h2 className='text-white md:text-left text-center font-bold text-[40px] md:text-[60px] leading-tight md:leading-[5rem]  w-[90%] md:w-4/5 md:mx-0 mx-auto'>
-                                            Collect All Your  <span className='text-[#97f675] text-center md:text-left'> Payments</span> with One <span className='c-home-title-underline'>Link</span>
+                                            <span ref={firstRef}> Collect All Your</span>
+                                            <span ref={secondRef} className='text-[#97f675] text-center md:text-left'> Payments </span> with One <span className='c-home-title-underline' ref={thirdRef}>Link</span>
                                         </h2>
-                                        <h4 className='text-white  md:text-left text-center w-[90%] md:w-4/5 md:mx-0 mx-auto'>All in one platform for accepting payment and embedded fintech experience to fast track your payment</h4>
+                                        <h4 className='text-white  md:text-left text-center w-[90%] md:w-4/5 md:mx-0 mx-auto' ref={typewriter}>All in one platform for accepting payment and embedded fintech experience to fast track your payment</h4>
                                     </div>
                                     <div className='py-5 mt-8 w-[90%] md:w-4/5 flex justify-center md:block md:mx-0 mx-auto'>
                                         <Link to='signup'>
-                                            <button className='bg-[#97f675] rounded-md py-4 px-12 font-bold'>Get Started</button>
+                                            <button ref={buttonRef} className='bg-[#97f675] rounded-md py-4 px-12 font-bold'>Get Started</button>
                                         </Link>
                                     </div>
                                 </Grid>
