9edd6de08beb674fdb6426bde606a53c8fb10a72
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-06-09 04:28:50 +0100

    add

--- src/pages/Waitlist.js
+++ src/pages/Waitlist.js
@@ -3,14 +3,14 @@ import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const Waitlist = () => {
-  const [fullName,setFullName] = useState('')
-  const [isEmail,setIsEmail] = useState()
-  const [validEmail,setValidEmail] = useState(false)
+  const [fullName, setFullName] = useState('')
+  const [isEmail, setIsEmail] = useState()
+  const [validEmail, setValidEmail] = useState(false)
 
-  const handleFullName  = (e)=>{
+  const handleFullName = (e) => {
     setFullName(e.target.value)
   }
-  const handleEmail  = (e)=>{
+  const handleEmail = (e) => {
     let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
     setIsEmail(e.target.value)
     setValidEmail(regex.test(e.target.value))
@@ -23,21 +23,24 @@ const Waitlist = () => {
             <img src='/favicon.svg' className='w-[100%] h-20' />
           </div>
           <div className='md:w-4/5 w-full mx-auto'>
-            <h2 className='text-[30px] md:text-[50px] text-center leading-none font-bold text-white'>Join the waitlist for <br />
-              <span className='text-[#97F675]'>Fourierpay</span></h2>
+            <div className='mb-10'>
+              <h2 className='text-[30px] md:text-[50px] text-center leading-none font-bold text-white'>Join the waitlist for <br />
+                <span className='text-[#97F675]'>Fourierpay</span></h2>
+            </div>
+
             <div className='md:w-4/5 w-full mx-auto py-6'>
               <form>
                 <div className='py-2 relative hover:text-white'>
-                  <PersonIcon className={fullName?'absolute top-4 left-2 text-white':'absolute top-4 left-2'} />
+                  <PersonIcon className={fullName ? 'absolute top-4 left-2 text-white' : 'absolute top-4 left-2'} />
                   <input className='py-3 w-full px-10 bg-[#16141c]  outline-none  text-white' placeholder='Full name...' onChange={handleFullName} />
                 </div>
                 <div className='py-3 relative hover:text-white'>
-                  <EmailIcon className={isEmail ? 'absolute top-6 left-2 text-white' :'absolute top-6 left-2' }/>
-                  <input className='py-3 w-full px-10 bg-[#16141c] outline-none text-white ' placeholder='Address email..' onChange={handleEmail}/>
+                  <EmailIcon className={isEmail ? 'absolute top-6 left-2 text-white' : 'absolute top-6 left-2'} />
+                  <input className='py-3 w-full px-10 bg-[#16141c] outline-none text-white ' placeholder='Address email..' onChange={handleEmail} />
                 </div>
                 <div className='py-5 relative hover:text-white'>
-                  <ArrowForwardIcon className={validEmail?'absolute top-8 right-8 text-white':'absolute top-8 right-8'}/>
-                  <button disabled={validEmail ? false:true } className='bg-[#16141c] py-3 px-5 rounded-md w-full text-white'>Join the waitlist</button>
+                  {/* <ArrowForwardIcon className={validEmail?'absolute top-8 right-8 text-white':'absolute top-8 right-8'}/> */}
+                  <button disabled={validEmail ? false : true} className='bg-[#16141c] py-3 px-5 rounded-md w-full text-white'>Join the waitlist</button>
                 </div>
               </form>
             </div>
