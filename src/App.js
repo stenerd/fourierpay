1e7bdefb18113254935be5b44bcbea6b08782ade
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-07-11 10:40:10 +0100

    register

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
       <Routes>
         <Route element={<ProtectedRoute />}>
           <Route exact path="/" element={<Home />} />
-          <Route path="/signup" element={<Navigate to="/login" />} />
-          {/* <Route path="/signup" element={<Signup />} /> */}
+          {/* <Route path="/signup" element={<Navigate to="/login" />} /> */}
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/about" element={<About/>}/>
