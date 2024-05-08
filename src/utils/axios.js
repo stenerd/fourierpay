535a1042d0dca92ff1473f503cb12394f9db1c0d
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-05-08 14:59:54 +0100

    main branches

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,9 +10,9 @@ const Protected = axios.create()
 
 // export const BASE_URL = `https://fourierpay.hostless.app`
 // export const BASE_URL = `http://localhost:9000`
-// export const BASE_URL =`https:fourierpayapi.onrender.com`
+export const BASE_URL =`https:fourierpayapi.onrender.com`
 
-export const BASE_URL = `https://fourierpayapi.herokuapp.com`
+// export const BASE_URL = `https://fourierpayapi.herokuapp.com`
 
 
 Protected.interceptors.request.use(
