f9453b869e1b157fa7801c61ca366cb52ff8358f
stephenkuf <stephensleem@gmail.com>
2024-05-27 20:41:25 +0100

    copy and input fixes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -148,8 +148,8 @@ const Signup = () => {
                                         </Grid>
 
                                         <div className=' mb-6'>
-                                            <p className='text-sm font-bold text-gray-500'> Signing into Fourier
-                                                <span className='c-primary-color'>pay</span> account means you agree to the
+                                            <p className='text-sm font-bold text-gray-500'> Signing up to 
+                                                <span className='c-primary-color'>Fourierpay</span> account means you agree to our
                                                 {/* <span className='c-primary-link-color'> Terms</span> and */}
                                                 <span className=''> Terms</span> and
                                                 <span className=''> Privacy Policy</span></p>
