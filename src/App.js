e4a184cb1ef31ea90a4794f39be1d8abee3b4541
stephenkuf <stephensleem@gmail.com>
2025-12-22 12:57:07 +0100

    affiliate routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import AffiliateSignup from './pages/AffiliateSignup'
+import AffiliateLogin from './pages/AffiliateLogin'
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
@@ -25,37 +27,39 @@ import FourierAgent from "./pages/Agent";
 
 function App() {
   return (
-    <div className="App overflow-hidden">
-      <Routes>
-        <Route element={<ProtectedRoute />}>
-          {/* <Route exact path="/" element={<Home />} /> */}
-          <Route exact path="/" element={<Login/>} />
-          {/* <Route path="/signup" element={<Navigate to="/login" />} /> */}
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/about" element={<About/>}/>
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-        </Route>
-        <Route path="/pay/:code" element={<MakePayment />} />
-        <Route path="/external-link/:code" element={<PublicPayment />} />
+		<div className="App overflow-hidden">
+			<Routes>
+				<Route element={<ProtectedRoute />}>
+					{/* <Route exact path="/" element={<Home />} /> */}
+					<Route exact path="/" element={<Login />} />
+					{/* <Route path="/signup" element={<Navigate to="/login" />} /> */}
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/affiliate-signup" element={<AffiliateSignup />} />
+					<Route path="/affiliate-login" element={<AffiliateLogin />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/reset-password/:token" element={<ResetPassword />} />
+				</Route>
+				<Route path="/pay/:code" element={<MakePayment />} />
+				<Route path="/external-link/:code" element={<PublicPayment />} />
 
-        <Route path="/reciept/:reference" element={<PaymentReciept />} />
-        <Route path='/admin/login' element={<AdminLogin />} />
-        <Route path='/waitlist' element={<Waitlist />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/profile" element={<Profile />} />
-          <Route path="/dashboard/transaction" element={<Transactions />} />
-          <Route path="/dashboard/paymentlinks" element={<PaymentLinks />} />
-          <Route path="/dashboard/payment" element={<Payment />} />
-          <Route path="/dashboard/agent" element={<FourierAgent />} />
-          <Route path="/dashboard/withdrawal" element={<Withdrawal />} />
-          <Route path="/dashboard/payment/:code" element={<SinglePaymentLink />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+				<Route path="/reciept/:reference" element={<PaymentReciept />} />
+				<Route path="/admin/login" element={<AdminLogin />} />
+				<Route path="/waitlist" element={<Waitlist />} />
+				<Route element={<PrivateRoutes />}>
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/dashboard/profile" element={<Profile />} />
+					<Route path="/dashboard/transaction" element={<Transactions />} />
+					<Route path="/dashboard/paymentlinks" element={<PaymentLinks />} />
+					<Route path="/dashboard/payment" element={<Payment />} />
+					<Route path="/dashboard/agent" element={<FourierAgent />} />
+					<Route path="/dashboard/withdrawal" element={<Withdrawal />} />
+					<Route path="/dashboard/payment/:code" element={<SinglePaymentLink />} />
+				</Route>
+			</Routes>
+		</div>
+	)
 }
 
 export default App;
